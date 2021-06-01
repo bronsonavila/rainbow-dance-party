373fce593d601c7360ee1ea0ba9fa40f572125fd
Bronson Avila <bronson.avila@bbot.menu>
2021-06-01 00:08:26 -1000

    Add og meta tags

--- pages/index.js
+++ pages/index.js
@@ -57,6 +57,14 @@ const ColorGrids = () => {
         <title>Rainbow Dance Party</title>
         <meta name="description" content="Rainbow Dance Party" />
         <meta name="author" content="Bronson Avila" />
+        <meta property="og:url" content="https://www.rainbowdance.party/" />
+        <meta
+          property="og:image"
+          content="https://www.rainbowdance.party/screenshot.png"
+        />
+        <meta property="og:site_name" content="Rainbow Dance Party" />
+        <meta property="og:title" content="Rainbow Dance Party" />
+        <meta property="og:description" content="Rainbow Dance Party" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
