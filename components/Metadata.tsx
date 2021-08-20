2ae8121d51b386c14d36444f81755acc7765b483
Bronson Avila <bronson.avila@bbot.menu>
2021-08-20 01:06:58 -1000

    <3 cookies

--- components/Metadata.tsx
+++ components/Metadata.tsx
@@ -5,6 +5,10 @@ const Metadata = () => (
     <title>Rainbow Dance Party</title>
     <meta name="description" content="Rainbow Dance Party" />
     <meta name="author" content="Bronson Avila" />
+    <meta
+      name="prominent-cookie-notification"
+      content="This website uses cookies to track everything you do. By visiting this website, you voluntarily consent to the collection, use, and storage of all of your personal identifying information — yesterday, today, and forever. You may opt out of the use of cookies by constructing a time machine and stopping your past self from having ever visited this website."
+    />
     <meta property="og:url" content="https://www.rainbowdance.party/" />
     <meta property="og:image" content="https://www.rainbowdance.party/screenshot.png" />
     <meta property="og:site_name" content="Rainbow Dance Party" />
