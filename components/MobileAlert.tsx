1216a5fa9cadddce08e4c2e23ed6016fbb40046d
Bronson Avila <bronson.avila@bbot.menu>
2021-08-20 01:27:44 -1000

    Fix MobileAlert re: type setting

--- components/MobileAlert.tsx
+++ components/MobileAlert.tsx
@@ -2,47 +2,47 @@ import { Alert } from 'antd'
 import { useEffect, useState } from 'react'
 
 const MobileAlert = () => {
-  const [alertHasBeenClosed, setAlertHasBeenClosed] = useState(null)
+  const [alertHasBeenClosed, setAlertHasBeenClosed] = useState<boolean | null>(null)
 
-  // Do not show the MobileAlert if the user has previously closed the warning notice.
   useEffect(() => {
-    setAlertHasBeenClosed(
-      typeof window !== 'undefined' && localStorage.getItem('alertHasBeenClosed')
-    )
+    if (typeof window !== 'undefined') {
+      setAlertHasBeenClosed(Boolean(localStorage.getItem('alertHasBeenClosed')))
+    }
   }, [])
 
+  // Do not show the MobileAlert if the user has previously closed the warning notice.
+  if (alertHasBeenClosed !== false) return null
+
   return (
-    !alertHasBeenClosed && (
-      <>
-        <Alert
-          closable
-          message="WARNING: Rainbow Dance Party consumes a large amount of system resources. The calculations required for each grid grow exponentially on each iteration. Please dance responsibly."
-          onClose={() => localStorage.setItem('alertHasBeenClosed', true)}
-          type="warning"
-        />
+    <>
+      <Alert
+        closable
+        message="WARNING: Rainbow Dance Party consumes a large amount of system resources. The calculations required for each grid grow exponentially on each iteration. Please dance responsibly."
+        onClose={() => localStorage.setItem('alertHasBeenClosed', 'true')}
+        type="warning"
+      />
 
-        <style global jsx>{`
+      <style global jsx>{`
+        .ant-alert {
+          margin: 1rem 1rem 0;
+        }
+
+        @media (min-width: 768px) {
           .ant-alert {
-            margin: 1rem 1rem 0;
+            display: none;
           }
+        }
 
-          @media (min-width: 768px) {
-            .ant-alert {
-              display: none;
-            }
-          }
+        .ant-alert-close-icon {
+          height: 100%;
+          margin-left: 1rem;
+        }
 
-          .ant-alert-close-icon {
-            height: 100%;
-            margin-left: 1rem;
-          }
-
-          .ant-alert-message {
-            font-size: 0.75rem;
-          }
-        `}</style>
-      </>
-    )
+        .ant-alert-message {
+          font-size: 0.75rem;
+        }
+      `}</style>
+    </>
   )
 }
 
